069e9bdea8bfe684279ff1bd6dd1159f7c6f2bee
spretosg <r.spielhofer@bluewin.ch>
2024-04-22 21:33:13 +0200

    add bigquery secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(project = "pareus",credentials=credentials)
 
 st.set_page_config(layout="wide")
 
