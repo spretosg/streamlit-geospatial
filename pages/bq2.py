2152c55c44d9d78e80538a25f0842eb49d5782c8
spretosg <r.spielhofer@bluewin.ch>
2024-05-02 12:22:41 +0200

    seems to work only with ee.Initi

--- pages/bq2.py
+++ pages/bq2.py
@@ -1,62 +1,34 @@
 import streamlit as st
-st.set_page_config(
-    page_title="streamlit-folium documentation: Draw Support",
-    page_icon=":pencil:",
-    layout="wide",
-)
-"""
-# streamlit-folium: Draw Support
-Folium supports some of the [most popular leaflet
-plugins](https://python-visualization.github.io/folium/plugins.html). In this example,
-we can add the
-[`Draw`](https://python-visualization.github.io/folium/plugins.html#folium.plugins.Draw)
-plugin to our map, which allows for drawing geometric shapes on the map.
-When a shape is drawn on the map, the coordinates that represent that shape are passed
-back as a geojson feature via the `all_drawings` and `last_active_drawing` data fields.
-Draw something below to see the return value back to Streamlit!
-"""
-with st.echo(code_location="below"):
-    import folium
-    import streamlit as st
-    from folium.plugins import Draw
-    import ee
-    from google.oauth2 import service_account
-    from google.cloud import bigquery
-
-    from streamlit_folium import st_folium
+import folium
+import streamlit as st
+from folium.plugins import Draw
+import ee
+from google.oauth2 import service_account
+from google.cloud import bigquery
+from streamlit_folium import st_folium
 
     # Create API client.
-    credentials = service_account.Credentials.from_service_account_info(
+credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
     )
-    client = bigquery.Client(credentials=credentials)
+ee.Initialize()
+client = bigquery.Client(credentials=credentials)
+rectangle = ee.Geometry.Rectangle(-88.09, 37.42, -122.08, 37.43)
 
-    rectangle = ee.Geometry.Rectangle(-122.09, 37.42, -122.08, 37.43)
+def main():
+    st.title('dummy data to bq')
+
+    rectangle = ee.Geometry.Rectangle(-88.09, 37.42, -122.08, 37.43)
 
     # Add the rectangle geometry to a FeatureCollection
     rectangle_fc = ee.FeatureCollection(rectangle)
-
     task = ee.batch.Export.table.toBigQuery(
       collection=rectangle_fc,
       table='pareus.earth_engine.mytable',
-      description='put_my_data_in_bigquery',
-      append=False)
+      description='test_task1',
+      append=True)
     task.start()
 
-    m = folium.Map(location=[64, 10], zoom_start=5)
-    Draw(
-        export=False,
-        position="topleft",
-        draw_options={
-            "polyline": False,
-            "poly": False,
-            "circle": False,
-            "polygon": False,
-            "marker": False,
-            "circlemarker": False,
-            "rectangle": True,
-        },
-    ).add_to(m)
-    #c1, c2 = st.columns(2)
-    c1 = st.columns(1)
-    c1 = st_folium(m, width=900, height=500)
+
+if __name__ == "__main__":
+    main()
