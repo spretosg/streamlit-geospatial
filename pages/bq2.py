da9b241e6124f32a29388ec3073c400a8feefd31
spretosg <r.spielhofer@bluewin.ch>
2024-05-03 13:16:58 +0200

    or the auth fkt

--- pages/bq2.py
+++ pages/bq2.py
@@ -13,22 +13,24 @@ credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
     )
 #ee.Initialize()
-geemap.ee_initialize()
+@st.cache_data
+def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
+    geemap.ee_initialize(token_name=token_name)
     
 
-rectangle = ee.Geometry.Rectangle(-88.09, 37.42, -122.08, 37.43)
+rectangle = ee.Geometry.Rectangle(-77.09, 37.42, -122.08, 37.43)
 
 def main():
     st.title('dummy data to bq')
 
-    rectangle = ee.Geometry.Rectangle(-88.09, 37.42, -122.08, 37.43)
+    rectangle = ee.Geometry.Rectangle(-77.09, 37.42, -122.08, 37.43)
 
     # Add the rectangle geometry to a FeatureCollection
     rectangle_fc = ee.FeatureCollection(rectangle)
     task = ee.batch.Export.table.toBigQuery(
       collection=rectangle_fc,
       table='pareus.earth_engine.mytable',
-      description='test_task1',
+      description='test_task3',
       append=True)
     task.start()
 
