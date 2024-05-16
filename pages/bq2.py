25ccdd66bfa29bbf7ab4f7c9b63ac6f35958f2cc
spretosg <r.spielhofer@bluewin.ch>
2024-05-16 12:44:55 +0200

    works locally but not on streamlit

--- pages/bq2.py
+++ pages/bq2.py
@@ -12,25 +12,26 @@ import geemap.foliumap as geemap
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
     )
-#ee.Initialize()
+geemap.ee_initialize()
 """ @st.cache_data
 def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
     geemap.ee_initialize(token_name=token_name) """
+
     
 
-rectangle = ee.Geometry.Rectangle(-66.09, 66.42, -122.08, 37.43)
+
 
 def main():
     st.title('dummy data to bq')
 
-    rectangle = ee.Geometry.Rectangle(-66.09, 66.42, -122.08, 37.43)
+    rectangle = ee.Geometry.Rectangle(-100.09, 66.42, -122.08, 37.43)
 
     # Add the rectangle geometry to a FeatureCollection
     rectangle_fc = ee.FeatureCollection(rectangle)
     task = ee.batch.Export.table.toBigQuery(
       collection=rectangle_fc,
       table='pareus.earth_engine.mytable',
-      description='test_task4',
+      description='test_task6',
       append=True)
     task.start()
 
